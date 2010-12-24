8a61beb1d255649102b5bb9396edb7730393197f
maxogden <max@maxogden.com>
2010-12-23 23:57:18 -0800

    initial checkin

---
+++ ezcrypto.js
@@ -0,0 +1,46 @@
+(function() {
+  var ezcrypto = this.ezcrypto = {};
+  
+  ezcrypto.generateKeys = function() {
+    var keys = RSAGenerate(ezcrypto.randomNumber());
+    return {'public': keys.n, 'private': keys.d};
+  }
+  
+  ezcrypto.encrypt = function(message, publicKey) {
+    var bigNumSessionKey = new BigInteger(128, 1, ezcrypto.randomNumber());
+    var sessionKey = bigNumSessionKey.toString(16);
+    var encryptedMessage = byteArrayToHex(rijndaelEncrypt(message, hexToByteArray(sessionKey), 'ECB'));
+    var encryptedKey = RSAEncrypt(sessionKey, publicKey);
+    return {'key': encryptedKey, 'message': encryptedMessage};
+  }
+  
+  ezcrypto.decrypt = function(encryptedMessage, publicKey, privateKey, encryptedKey) {
+    var decryptedKey = RSADecrypt(encryptedKey, publicKey, privateKey);
+    var decryptedMessage = byteArrayToString(rijndaelDecrypt(hexToByteArray(encryptedMessage), hexToByteArray(decryptedKey), 'ECB'));
+    return decryptedMessage;
+  }
+  
+  ezcrypto.randomNumber = function() {
+    return new SecureRandom();
+  }
+  
+  function load(scripts) {
+    for (var i=0; i < scripts.length; i++) {
+      document.write('<script src="'+scripts[i]+'"><\/script>')
+    };
+  };
+
+  load([
+    "vendor/pidcrypt.js",
+    "vendor/pidcrypt_util.js",
+    "vendor/asn1.js",
+    "vendor/jsbn.js",
+    "vendor/rng.js",
+    "vendor/prng4.js",
+    "vendor/rsa.js",
+    "vendor/genkey.js",
+    "vendor/rijndael.js",
+    "vendor/custom.js"
+  ]);
+  
+})();
